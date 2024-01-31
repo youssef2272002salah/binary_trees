c654832caa74f96d1fa736cd60175d2405c8c700
youssef2272002salah <youssef22772002salah@gmail.com>
2024-01-31 19:02:48 +0200

    3

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,12 +2,13 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
     {
+          binary_tree_t * new_node; 
         if (parent == NULL)
             {
                 return (NULL);
             }
-        
-        binary_tree_t * new_node = binary_tree_node(parent,value);
+
+        new_node = binary_tree_node(parent,value);
         
         if (new_node == NULL)
             {
