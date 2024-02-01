8b6d21fa4f7e0f8f1eed89e4e8a60bf63a8b6812
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-02-01 18:32:00 +0200

    "hotfix"

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -34,7 +34,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (tree->left)
-		l = binary_tree_height(tree->left) + 1; 
+		l = binary_tree_height(tree->left) + 1;
 	if (tree->right)
 		r = binary_tree_height(tree->right) + 1;
 	return (l - r);
