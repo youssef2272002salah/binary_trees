1f0cd260c701aa8cb5f2e029fa00a7941c1ba551
youssef2272002salah <youssef22772002salah@gmail.com>
2024-02-01 16:12:37 +0200

    barnch

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,8 +12,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
         int maxx = 0;
         if (tree == NULL)
             return 0;
+
         else             
             {
+                // 55555555555555555
                 if(tree)
                     maxx = max(((tree->left)?1:0) + binary_tree_height(tree->left),((tree->right)?1:0) +binary_tree_height(tree->right));
                 return maxx;
