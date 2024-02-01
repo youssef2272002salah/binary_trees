a46f6c332d538e5d9ed1aa2691404013db71b7de
youssef2272002salah <youssef22772002salah@gmail.com>
2024-02-01 18:00:54 +0200

    add size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -12,9 +12,9 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-		x = binary_tree_size(tree->right);
-		y = binary_tree_size(tree->left);
-		return (1 + x + y);
+	x = binary_tree_size(tree->right);
+	y = binary_tree_size(tree->left);
+	return (1 + x + y);
 
 }
 
