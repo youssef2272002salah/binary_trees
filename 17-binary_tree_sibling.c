d35c59dc46b27aa6f67e6402823cb9c0450a8230
youssef2272002salah <youssef22772002salah@gmail.com>
2024-02-01 18:10:39 +0200

    finish

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,23 +1,29 @@
 #include "binary_trees.h"
 
+/**
+ *binary_tree_sibling - function to determine the sibling of a node
+ *@tree: tree to check
+ *Return: the sibling of a node
+*/
+
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
-    {
-        if(node == NULL)
-            return (NULL);
-        if (node->parent == NULL)
-            return (NULL);
-        if(node->parent->left == node)
-            {
-                if(node->parent->right)
-                    return node->parent->right;
-                else 
-                    return (NULL);
-            }
-        else
-            {
-                if(node->parent->left)
-                    return node->parent->left;
-                else 
-                    return (NULL);
-            }   
-    }
+{
+	if (node == NULL)
+		return (NULL);
+	if (node->parent == NULL)
+		return (NULL);
+	if (node->parent->left == node)
+	{
+		if (node->parent->right)
+			return (node->parent->right);
+		else
+			return (NULL);
+	}
+	else
+	{
+		if (node->parent->left)
+			return (node->parent->left);
+		else
+			return (NULL);
+	}
+}
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,12 +1,21 @@
 #include "binary_trees.h"
 
+/**
+ *binary_tree_uncle - calculate the uncle of a node
+ *
+ *@tree : the root
+ *
+ *Return: the uncle of a node
+ *
+ */
+
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
-    {
-        if(node == NULL)
-            return (NULL);
-        if (node->parent == NULL || node->parent->parent == NULL)
-            return (NULL);
-        if(node->parent->parent->left == node->parent)
-            return node->parent->parent->right;
-        return node->parent->parent->left;
-    }
+{
+	if (node == NULL)
+		return (NULL);
+	if (node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
+}
